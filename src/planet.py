bc1830b6cdcffcd861e5bce9daaaf52ff2093146
Adam Neeley <adam.t.neeley@protonmail.com>
2024-11-22 22:09:51 -0800

    load planet images in assets

--- src/planet.py
+++ src/planet.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 import pygame
 import math
+from assets import planet_images
 
 
 class Planet(pygame.sprite.Sprite):
     def __init__(self, name, radius, orbit_radius, speed):
         super().__init__()
         self.name = name
-        self.image = pygame.image.load(f"assets/{name}.png")
+        self.image = planet_images[name]
         self.image = pygame.transform.scale(self.image, (radius * 2, radius * 2))
         self.rect = self.image.get_rect()
         self.angle = 0
