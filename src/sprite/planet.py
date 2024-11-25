30dffb643c35395f4513e65d44ac349082c08176
Adam Neeley <adam.t.neeley@protonmail.com>
2024-11-25 14:54:17 -0800

    planet sprite

--- src/sprite/planet.py
+++ src/sprite/planet.py
@@ -1,5 +1,7 @@
 import math
 
+from src.asset.color import Color
+from src.asset.font import text_font
 import numpy
 import numpy as np
 import pygame
@@ -14,33 +16,31 @@ class PlanetSprite(pygame.sprite.Sprite):
     def __init__(self, name, radius, orbit_radius, speed, **kwargs):
         super().__init__()
         self.pos = np.array([orbit_radius * 2, 0, 0])
-        self.pos = self.pos
         self.radius = radius * 2
         self.speed = speed
         self.image = planet_images[name]
+        self.name_text = text_font.render(name, False, Color.text)
 
     def update(self):
         self.pos = rotate_z(self.pos, self.speed * 0.5)
         norm = np.linalg.norm(self.pos)
         size = (1 + self.pos[2] / norm) * 2 * self.radius
-        self.rect = pygame.Rect(
-            (self.pos[0] - self.radius, self.pos[1] - self.radius),
-            (size, size),
-        )
+        # self.rect = pygame.Rect(
+        #     (self.pos[0] - self.radius, self.pos[1] - self.radius),
+        #     (size, size),
+        # )
 
     def draw(self, surface):
         pos = rotate_x(self.pos, 80)
-        # pygame.draw.circle(
-        #     surface,
-        #     (255, 255, 255),
-        #     (pos[0] + 400, pos[1] + 300),
-        #     self.radius,
-        # )
         self.image = pygame.transform.scale(
             self.image,
             (self.radius * 2, self.radius * 2),
         )
+        center = (
+            pos[0] + 400 - self.radius,
+            pos[1] + 300 - self.radius,
+        )
+        surface.blit(self.image, center)
         surface.blit(
-            self.image,
-            (pos[0] + 400 - self.radius, pos[1] + 300 - self.radius),
+            self.name_text, (center[0] + self.radius * 0, center[1] + self.radius * 2.5)
         )
