d2693acb62fc3b7c9c1aa67760388316a08656df
Adam Neeley <adam.t.neeley@protonmail.com>
2024-12-09 17:51:03 -0800

    animate bg stars

--- planetclicker/sprite/bg_star.py
+++ planetclicker/sprite/bg_star.py
@@ -1,3 +1,5 @@
+import numpy as np
+from pygame.surface import Surface
 import random
 
 import pygame
@@ -9,22 +11,45 @@ class BGStar:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.dx = random.uniform(-1, 1)
-        self.dy = random.uniform(-1, 1)
-        self.surface = pygame.Surface((1, 1))
-        self.surface.set_alpha(random.randint(0, 200))
+        self.z = np.random.uniform(0.0, 10.0)
+        self.r = 0.5 / self.z
+        # self.r = np.random.geometric(0.618)
+        self.t = np.random.uniform(0.0001, 2.0 * np.pi)
+        self.dt = np.random.uniform(-0.01, 0.01)
+        self.dx = self.r * np.cos(self.t)
+        self.dy = self.r * np.sin(self.t)
+        self.dz = self.r * np.cos(self.t)
+        self.surface = pygame.Surface((self.r, self.r))
 
     def update(self):
-        self.x += self.dx / 10
-        self.y += self.dy / 10
+        self.t += self.dt
+        self.dx = self.r * np.cos(self.t)
+        self.dy = self.r * np.sin(self.t)
+        # self.dz = np.sin(self.t / 2.0)
+        self.x += self.dx / 1
+        self.y += self.dy / 1
+        # self.z = np.sin(self.t / 10.0)
 
-    def draw(self, window):
-        self.surface.fill(Colors.text)
-        window.blit(self.surface, (self.x, self.y))
+    def draw(self, surface: Surface):
+        pygame.draw.circle(
+            surface,
+            color=Colors.accent,
+            center=(self.x, self.y),
+            radius=0.5 + 1.0 / self.z,
+            # radius=1 / (np.square(self.z)),
+        )
+        self.surface.set_alpha(int(10.0 + self.z))
+        print("z", 20.0 / self.z)
+        # surface.blit(
+        #     self.surface,
+        #     (self.x, self.y),
+        # )
+        # self.surface.fill(Colors.brand)
+        # window.blit(self.surface, (self.x, self.y, 5, 5))
 
 
 class BGStarSystem:
-    def __init__(self, n=100, size=(800, 600)):
+    def __init__(self, n=100, size=(1600, 900)):
         self.size = size
         self.surface = pygame.Surface(size)
 
@@ -44,5 +69,11 @@ class BGStarSystem:
     def draw(self, surface):
         self.surface.fill(pygame.SRCALPHA)
         for particle in self.particles:
+            # pygame.draw.circle(
+            #     self.surface,
+            #     color=Colors.accent,
+            #     center=(particle.x, particle.y),
+            #     radius=int(particle.r),
+            # )
             particle.draw(self.surface)
         surface.blit(self.surface, (0, 0))
